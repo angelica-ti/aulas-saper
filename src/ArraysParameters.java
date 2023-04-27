7c589256e1b138e47b085d5bc7acd0d799274a0b
Angélica <angelica.viana.ti@gmail.com>
2023-04-27 20:59:37 -0300

    Create class Student2 and Adapt ArrayAverageStudents

---
+++ src/ArraysParameters.java
@@ -0,0 +1,23 @@
+public class ArraysParameters {
+    public static void main(String[] args){
+        int[] array = {1, 2, 3, 4, 5};
+        multiplyArrayBy2(array);
+        System.out.printf("The values of the modified array are:%n");
+        for (int value : array){
+            System.out.printf("%d ", value);
+        }
+
+        multiplyArrayBy2(array);
+        System.out.printf("%n");
+        System.out.printf("The values of the modified array are:%n");
+        for (int value : array){
+            System.out.printf("%d ", value);
+        }
+    }
+    private static void multiplyArrayBy2(int[] oldArray){
+        for(int counter = 0; counter < oldArray.length ; counter++){
+            oldArray[counter] *= 2;
+        }
+    }
+}
+
