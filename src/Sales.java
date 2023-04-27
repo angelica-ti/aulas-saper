6ffac8d9e26074fe5be94fae3ba3379369e1493a
Angélica <angelica.viana.ti@gmail.com>
2023-04-27 09:38:51 -0300

    Exercises about Arrays

--- src/Sales.java
+++ src/Sales.java
@@ -3,6 +3,9 @@ Faça um programa que mostra o valor das vendas de uma loja e
 a média de vendas de todos os vendedores. O usuário do programa
 deve fornecer a quantidade de vendedores da loja e o valor das vendas
 de cada vendedor.
+
+Modifique o programa anterior para mostrar o maior valor de vendas e o
+número do vendedor que mais vendeu.
 */
 import java.util.Scanner;
 
@@ -14,14 +17,23 @@ public class Sales {
         int qtySalers = input.nextInt();
 
         double sales, totalSales = 0;
+        double max_sale = Double.NEGATIVE_INFINITY;
+        int max_saler = 1;
 
         for(int i = 1; i <= qtySalers; i++){
             System.out.printf("Valor das vendas do %d° vendedor: ", i);
             sales = input.nextDouble();
             totalSales += sales;
+
+            if(sales > max_sale){
+                max_sale = sales;
+                max_saler = i;
+            }
+
         }
 
         System.out.printf("Valor total das vendas: R$ %.2f%n", totalSales);
-        System.out.printf("Média das vendas de todos os vendedores: R$ %.2f", totalSales/qtySalers);
+        System.out.printf("Média das vendas de todos os vendedores: R$ %.2f%n", totalSales/qtySalers);
+        System.out.printf("O vendedor %d teve o maior valor de vendas: R$ %.2f", max_saler, max_sale);
     }
 }
