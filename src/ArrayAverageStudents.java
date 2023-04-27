b302187687fbce6dd6c0e320db566f9ca253199f
Angélica <angelica.viana.ti@gmail.com>
2023-04-27 20:27:50 -0300

    Create class Student2 and Adapt ArrayAverageStudents

--- src/ArrayAverageStudents.java
+++ src/ArrayAverageStudents.java
@@ -7,26 +7,39 @@ import java.util.Scanner;
 
 public class ArrayAverageStudents {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
+        Scanner inputNumbers = new Scanner(System.in);
+        Scanner inputStrings = new Scanner(System.in);
         System.out.print("Digite o número de estudantes da turma: ");
-        int numberStudents = input.nextInt();
+        int numberStudents = inputNumbers.nextInt();
 
-        double[] grades = new double[numberStudents];
+        Student2[] students = new Student2[numberStudents];
 
-        double average = 0;
-        for(int i = 0; i < grades.length; i++){
-            System.out.printf("Digite a nota do aluno %d: ", i+1);
-            grades[i] = input.nextDouble();
-            average += grades[i];
+        for(int i = 0; i < students.length; i++){
+            System.out.printf("Digite o nome do aluno %d: ", i+1);
+            String name = inputStrings.nextLine();
+
+            System.out.printf("Digite a nota 1 do aluno %d: ", i+1);
+            double grade1 = inputNumbers.nextDouble();
+
+            System.out.printf("Digite a nota 2 do aluno %d: ", i+1);
+            double grade2 = inputNumbers.nextDouble();
+
+            students[i] = new Student2(name, grade1, grade2);
         }
 
-        average /= grades.length;
-        System.out.printf("A média dos alunos é %.2f%n", average);
+        double[] averages = new double[numberStudents];
+        double sumAverages = 0;
 
+        for(int i = 0; i < students.length; i++){
+            averages[i] = students[i].average();
+            sumAverages += averages[i];
+        }
+
+        double averageTeam = sumAverages/numberStudents;
         int counter = 0;
-        for(int i = 0; i < grades.length; i++){
-            if(grades[i] >= average){
-                System.out.printf("O %d° aluno teve a nota maior que a média da turma: %.2f%n", i+1, grades[i]);
+        for(int i = 0; i < students.length; i++){
+            if(students[i].average() >= averageTeam){
+                System.out.printf("O %d° aluno teve a media maior que a média da turma: %.2f%n", i+1, averages[i]);
                 counter++;
             }
         }
